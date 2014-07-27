2f85223ab0a7695188c8c101e7e2295023d7378c
Jenny Duckett <jenny@cleanercode.com>
2014-07-27 19:28:01 +0100

    Tidy up cloning of repo

--- fabfile.py
+++ fabfile.py
@@ -18,12 +18,14 @@ def deploy(committish):
     with settings(sudo_user=unix_user):
         sudo("ls /var/www")
         timestamp = datetime.utcnow().replace(microsecond=0).isoformat()
-        release_dir = os.path.join(site_root, 'releases', timestamp)
-        sudo('mkdir -p {}'.format(release_dir))
+        new_release_dir = os.path.join(site_root, 'releases', timestamp)
+        new_repo_dir = os.path.join(new_release_dir, 'example-repository')
+        sudo('mkdir -p {}'.format(new_release_dir))
 
-        with cd(release_dir):
+        with cd(new_release_dir):
             # Assume that the user has a deploy key for this repo - this
             # should be created by puppet and manually added to the repo
-            sudo('git clone git@github.com:mhl/example-repository.git')
-            with cd(os.path.join(release_dir, 'example-repository')):
-                sudo('git checkout {}'.format(committish))
+            sudo('git clone -q git@github.com:mhl/example-repository.git')
+
+        with cd(new_repo_dir):
+            sudo('git checkout {}'.format(committish))
