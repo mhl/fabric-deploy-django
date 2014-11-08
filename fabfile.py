9d23ca0fa469669ca9bad6066521014f4fed5101
Mark Longair <mhl@pobox.com>
2014-11-08 10:45:33 +0000

    Save bandwidth, time and disk space by mirroring an existing clone
    The foundation submodule in particular is huge to be recloning on every
    deploy, but in any case it's wasteful not to reuse the object databases
    from a previous deploy before fetching anything missing.
    
    This commit tries to clone the repository from the last successful
    deploy, and otherwise clones from GitHub as usual.

--- fabfile.py
+++ fabfile.py
@@ -26,13 +26,22 @@ class DeployTask(Task):
             current_repo_dir = join(current_release_dir, 'example-repository')
             sudo('mkdir -p {}'.format(new_release_dir))
 
-            with cd(new_release_dir):
-                # Assume that the user has a deploy key for this repo - this
-                # should be created by puppet and manually added to the repo
-                sudo('git clone -q --recursive git@github.com:mhl/example-repository.git')
+            # Assume that the user has a deploy key for this repo - this
+            # should be created by puppet and manually added to the repo
+            if 0 == sudo('test -d {0}'.format(current_repo_dir), quiet=True).return_code:
+                # In this case a previous deploy had succeeding in
+                # cloning the repository, so we can save time and
+                # bandwidth on each deploy by basing our new clone
+                # on that
+                self.local_clone(current_repo_dir, new_repo_dir)
+            else:
+                with cd(new_release_dir):
+                    # Otherwise, we need to clone everything from scratch:
+                    sudo('git clone -q --recursive git@github.com:mhl/example-repository.git')
+
+            self.checkout_committish(new_repo_dir, committish)
 
             with cd(new_repo_dir):
-                sudo('git checkout {}'.format(committish))
                 short_commit = sudo('git rev-parse --short HEAD').strip()
 
             new_requirements = join(new_repo_dir, 'requirements.txt')
@@ -79,10 +88,87 @@ class DeployTask(Task):
                 with cd(new_repo_dir):
                     sudo('git push -f origin {}:refs/heads/deployed'.format(committish))
 
-
     def requirements_changed(self, current, new):
         diff_command = "diff {} {}".format(current, new)
         diff = sudo(diff_command, quiet=True)
         return diff.return_code != 0
+
+    def local_clone(self, source_repo, destination_repo):
+        '''Efficiently mirror a local non-bare repository
+
+        This will clone source_repo to destination_repo (using
+        hard-links in the object database, so it's efficient in disk
+        space terms) but make sure that the origin remote's URL in
+        destination_repo is the same as in source_repo (rather than it
+        pointing to source_repo). It also recursively mirrors
+        submodules from the existing respository.'''
+
+        with settings(sudo_user=unix_user):
+            sudo('git clone {0} {1}'.format(
+                source_repo,
+                destination_repo
+            ))
+            # Save the real origin URL of this repository:
+            with cd(source_repo):
+                origin_url = sudo('git config remote.origin.url')
+            with cd(destination_repo):
+                sudo('git remote set-url origin {0}'.format(origin_url))
+                sudo('git submodule init')
+                for line in sudo('git submodule status').splitlines():
+                    fields = line.strip().split()
+                    path = fields[1]
+                    source_submodule = join(source_repo, path)
+                    destination_submodule = join(destination_repo, path)
+                    self.local_clone(source_submodule, destination_submodule)
+
+    def checkout_committish(self, repo_directory, committish):
+        '''Switch the given non-bare repository to committish
+
+        The reason this is more complicated than:
+
+            git checkout [committish]
+
+        ... is that in every case we want to make sure it's
+        up-to-date; in the case of a local branch that means fetching,
+        and resetting it to its upstream branch.'''
+
+        with settings(sudo_user=unix_user):
+            with cd(repo_directory):
+                # First, make sure that all the remote-tracking
+                # branches are up-to-date:
+                sudo('git fetch origin')
+                # Try to resolve committish as a local branch:
+                output = sudo('git rev-parse --verify refs/heads/{0}'.format(committish), quiet=True)
+                if output.return_code == 0:
+                    # Then committish is a local branch, and we
+                    # can check it out and reset it to upstream:
+                    sudo('git checkout {0}'.format(committish))
+                    sudo('git reset --hard {0}@{{upstream}}'.format(committish))
+                else:
+                    # Then committish is presumably either:
+                    #
+                    #  (a) A remote-tracking branch, a tag, or an
+                    #      object name (SHA1sum)
+                    #
+                    #  (b) A name ('foo', say) that corresponds to
+                    #      a remote-tracking branch origin/foo
+                    #
+                    #  (c) Something else
+                    #
+                    # In the case of (a) this checkout will detach
+                    # HEAD and point it to the committish - if it's a
+                    # remote-tracking branch that will have been
+                    # updated above.
+                    #
+                    # In the case of (b) this will use checkout's
+                    # DWIM ('Do What I Mean') behaviour, creating
+                    # a local branch called 'foo' based on
+                    # 'origin/foo' and setting origin/foo as its
+                    # upstream.
+                    #
+                    # In the case of (c) this will just fail and
+                    # that's the best we can do.
+                    sudo('git checkout {0}'.format(committish))
+                sudo('git submodule update --init')
 
 deploy = DeployTask()
