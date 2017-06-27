e2a023c060a6c185ce0f1ffd6ac958a0de246f5a
Mark Longair <mhl@pobox.com>
2017-06-27 20:58:04 +0100

    Add a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "fabric-deploy-django",
+    version = "0.0.1",
+    packages = find_packages(),
+    author = "Mark Longair",
+    author_email = "mhl@pobox.com",
+    description = "A Fabric base class for deploying Django sites",
+    license = "MIT",
+    keywords = "django fabric deployment",
+    install_requires = [
+        'Fabric',
+        'fabric-virtualenv',
+    ]
+)
